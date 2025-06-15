c2aaae32716015e74aa17aa516d2b1f4f7e616d4
mahmoudelsawy99 <mahmoudelsawy048@gmail.com>
2025-06-15 17:17:49 +0300

    fix responsive and timer with interval

--- src/app/components/comming-soon/comming-soon.component.ts
+++ src/app/components/comming-soon/comming-soon.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comming-soon',
   templateUrl: './comming-soon.component.html',
   styleUrls: ['./comming-soon.component.scss']
 })
-export class CommingSoonComponent {
+export class CommingSoonComponent implements OnInit, OnDestroy {
+targetDate = new Date(Date.now() + 11 * 24 * 60 * 60 * 1000);
+  intervalId: any;
 
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
+  ngOnInit() {
+    this.updateTimer();
+    this.intervalId = setInterval(() => this.updateTimer(), 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  updateTimer() {
+    const now = new Date().getTime();
+    const distance = this.targetDate.getTime() - now;
+
+    if (distance < 0) {
+      clearInterval(this.intervalId);
+      this.days = this.hours = this.minutes = this.seconds = 0;
+      return;
+    }
+
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  }
 }
